b92d378741eb54ab09d183d45ea2dbc3d17e9602
ritesh singh <trex@riteshs-MacBook-Pro.local>
2024-03-12 14:22:23 +0530

    added cube

--- src/modules/playground/components/simulated-hardwares/cube.tsx
+++ src/modules/playground/components/simulated-hardwares/cube.tsx
@@ -10,7 +10,15 @@ const COMPONENT_KEY = 'CUBE';
 
 export const Cube: FC = () => {
     const cubeRef = useRef();
-
+// An array of colors for each face of the cube
+    const colors = [
+        'red',    // +X (right)
+        'green',  // -X (left)
+        'blue',   // +Y (top)
+        'yellow', // -Y (bottom)
+        'orange', // +Z (front)
+        'red'  // -Z (back)
+    ];
     const [state, setState] = useState<CubeStateType>({
         motion: 'idle'
     });
@@ -25,6 +33,8 @@ export const Cube: FC = () => {
             case 'FORWARD':
                 // @ts-ignore
                 cubeRef.current.position.x += speed;
+                // @ts-ignore
+                // cubeRef.current.rotation.y += rotationSpeed;
                 setState({motion: 'idle'})
                 break;
             case 'backward':
@@ -51,8 +61,11 @@ export const Cube: FC = () => {
 
     return (
         <mesh ref={cubeRef}>
-            <boxGeometry args={[1, 1, 1]}/>
-            <meshStandardMaterial color="black"/>
+            <boxGeometry args={[3, 3, 3]} />
+            {/* Apply different materials for each face */}
+            {colors.map((color, index) => (
+                <meshStandardMaterial key={index} color={color} />
+            ))}
         </mesh>
     );
 
