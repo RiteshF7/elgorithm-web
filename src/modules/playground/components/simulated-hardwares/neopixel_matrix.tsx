979d2961293059757700d6438081a5420a43c658
ritesh singh <trex@riteshs-MacBook-Pro.local>
2024-03-16 14:52:43 +0530

    added neopixel with state management

--- src/modules/playground/components/simulated-hardwares/neopixel_matrix.tsx
+++ src/modules/playground/components/simulated-hardwares/neopixel_matrix.tsx
@@ -1,22 +1,31 @@
 'use client';
 
-import {FC, useEffect, useState} from "react";
+import React, {FC, useEffect, useRef, useState} from "react";
 import {usePlayground} from "@/modules/playground/providers/playground.provider";
 import '@wokwi/elements';
+import {RGB} from "@wokwi/elements/dist/cjs/types/rgb";
+import {NeopixelMatrixElement} from "@wokwi/elements/dist/esm/neopixel-matrix-element";
 
 
 interface NeoPixelMatrixStateType {
-    active?: boolean;
-    color?: string;
+    row: number;
+    column: number;
+    rgb: RGB;
 }
 
 const COMPONENT_KEY = 'NEO_PIXEL_MATRIX';
 
 export const NeoPixelMatrix: FC = () => {
 
+    const neoPixelDisplayRef = useRef<NeopixelMatrixElement>();
+
     const [state, setState] = useState<NeoPixelMatrixStateType>({
-        active: false,color: 'red'
+        row: 0, column: 0,
+        rgb: {r: 0, g: 0, b: 0},
+
     });
+
+
 
     const {registerComponent} = usePlayground();
 
@@ -25,13 +34,22 @@ export const NeoPixelMatrix: FC = () => {
         registerComponent(COMPONENT_KEY, (data) => {
             console.log('Updated NeoPixelMatrix data', data);
             setState((state) => ({...state, ...data}))
+            neoPixelDisplayRef.current?.setPixel(state.row, state.column, state.rgb);
         })
     }, []);
+
     return (
 
-        <div className={'flex flex-row gap-4  items-center bg-black p-4'}>
-            {//@ts-ignore
-            <wokwi-neopixel-matrix rows="16" cols="16"></wokwi-neopixel-matrix>
+        <div className={' bg-black flex flex-col gap-4 p-2'}>
+            {/*<wokwi-neopixel-matrix rows={8} cols={8} rowSpacing={2} colSpacing={2} animation={true} />*/}
+            {// @ts-ignore
+            <wokwi-neopixel-matrix ref={neoPixelDisplayRef} cols={8} blurLight={true} ></wokwi-neopixel-matrix>
             }
-        </div>)
+            <button className={'bg-amber-400 w-10 h-10'} onClick={() => {
+                neoPixelDisplayRef.current?.setPixel(2, 3, {r: 225, g: 0, b: 0})
+                console.log('Updated NeoPixelMatrix data');
+
+            }}></button>
+
+        </div>);
 }
