b4e18045198ad9474e6d8f4f8d7422058d09aea1
ritesh singh <trex@riteshs-MacBook-Pro.local>
2024-03-18 20:19:16 +0530

    lint

--- src/modules/playground/components/simulated-hardwares/neopixel-display/NeoPixelMatrix.tsx
+++ src/modules/playground/components/simulated-hardwares/neopixel-display/NeoPixelMatrix.tsx
@@ -1,9 +1,9 @@
 // NeoPixelMatrix.tsx
-import React, { FC, useEffect, useRef, useState } from "react";
-import { usePlayground } from "@/modules/playground/providers/playground.provider";
+import React, {FC, useEffect, useRef, useState} from "react";
+import {usePlayground} from "@/modules/playground/providers/playground.provider";
 import '@wokwi/elements';
-import { Position, Direction } from "./types";
-import { calculateMove, isValidPosition } from "./NeoPixelUtils";
+import {Position, Direction} from "./types";
+import {calculateMove, isValidPosition} from "./NeoPixelUtils";
 import {NeopixelMatrixElement} from "@wokwi/elements";
 
 interface NeoPixelMatrixProps {
@@ -23,19 +23,19 @@ export const NeoPixelMatrix: FC<NeoPixelMatrixProps> = ({
                                                             destinationPosition,
                                                             matrixSize,
                                                         }) => {
-    const { registerComponent } = usePlayground();
+    const {registerComponent} = usePlayground();
     const neoPixelDisplayRef = useRef<NeopixelMatrixElement>(null);
     const [state, setState] = useState<NeoPixelMatrixStateType>({
         Direction: Direction.Stop,
     });
 
-    const position = { ...startingPosition };
+    const position = {...startingPosition};
 
     useEffect(() => {
         setInitialPixel();
         setDestinationPixel();
         registerComponent(COMPONENT_KEY, (data) => {
-            setState((state) => ({ ...state, ...data }));
+            setState((state) => ({...state, ...data}));
             move(data.Direction);
         });
     }, []);
@@ -63,12 +63,13 @@ export const NeoPixelMatrix: FC<NeoPixelMatrixProps> = ({
     }
 
     function setPixel(position: Position) {
-        neoPixelDisplayRef.current?.setPixel(position.row, position.column, { r: 225, g: 0, b: 0 });
+        neoPixelDisplayRef.current?.setPixel(position.row, position.column, {r: 225, g: 0, b: 0});
     }
 
     return (
         <div className={' bg-black flex flex-col gap-4 p-2'}>
-            <wokwi-neopixel-matrix ref={neoPixelDisplayRef} rows={matrixSize} cols={matrixSize} blurLight={true}></wokwi-neopixel-matrix>
+            <wokwi-neopixel-matrix ref={neoPixelDisplayRef} rows={matrixSize} cols={matrixSize}
+                                   blurLight={true}></wokwi-neopixel-matrix>
             <div className={'flex flex-row gap-2 justify-center'}>
                 <button className={'bg-amber-400 w-10 h-10'} onClick={() => move(Direction.TopLeft)}>TL</button>
                 <button className={'bg-amber-400 w-10 h-10'} onClick={() => move(Direction.TopRight)}>TR</button>
