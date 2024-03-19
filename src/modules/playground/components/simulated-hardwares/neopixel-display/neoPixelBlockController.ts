ddd3cba259963de8c39a771bf1d2cf03695a4f3e
ritesh singh <trex@riteshs-MBP.bbrouter>
2024-03-19 12:50:07 +0530

    optimzation

--- src/modules/playground/components/simulated-hardwares/neopixel-display/neoPixelBlockController.ts
+++ src/modules/playground/components/simulated-hardwares/neopixel-display/neoPixelBlockController.ts
@@ -1,19 +1,22 @@
 import {COMPONENT_KEY} from "@/modules/playground/components/simulated-hardwares/neopixel-display/NeoPixelMatrix";
-import {getChannelMessage,getChannelMessageWithDelay} from "@/utils/pg-comm-channel.util";
+import {getChannelMessageWithDelay} from "@/utils/pg-comm-channel.util";
 import {Direction} from "@/modules/playground/components/simulated-hardwares/neopixel-display/types";
 
-const NEO_PIXEL_COMPONENT_KEY = COMPONENT_KEY ;
 
 const NeoPixelController = {
-    moveUp: () => getChannelMessageWithDelay(NEO_PIXEL_COMPONENT_KEY, { Direction: Direction.Up }),
-    moveDown: () => getChannelMessageWithDelay(NEO_PIXEL_COMPONENT_KEY, { Direction: Direction.Down }),
-    moveLeft: () => getChannelMessageWithDelay(NEO_PIXEL_COMPONENT_KEY, { Direction: Direction.Left }),
-    moveRight: () => getChannelMessageWithDelay(NEO_PIXEL_COMPONENT_KEY, { Direction: Direction.Right }),
-    moveTopLeft: () => getChannelMessageWithDelay(NEO_PIXEL_COMPONENT_KEY, { Direction: Direction.TopLeft }),
-    moveTopRight: () => getChannelMessageWithDelay(NEO_PIXEL_COMPONENT_KEY, { Direction: Direction.TopRight }),
-    moveBottomLeft: () => getChannelMessageWithDelay(NEO_PIXEL_COMPONENT_KEY, { Direction: Direction.BottomLeft }),
-    moveBottomRight: () => getChannelMessageWithDelay(NEO_PIXEL_COMPONENT_KEY, { Direction: Direction.BottomRight }),
-    stop: () => getChannelMessage(NEO_PIXEL_COMPONENT_KEY, { Direction: Direction.Stop })
+    moveUp: () => getDirectionMessage( Direction.Up ),
+    moveDown: () => getDirectionMessage(Direction.Down ),
+    moveLeft: () => getDirectionMessage( Direction.Left ),
+    moveRight: () => getDirectionMessage(Direction.Right ),
+    moveTopLeft: () => getDirectionMessage(Direction.TopLeft ),
+    moveTopRight: () => getDirectionMessage(Direction.TopRight ),
+    moveBottomLeft: () => getDirectionMessage(Direction.BottomLeft ),
+    moveBottomRight: () => getDirectionMessage(Direction.BottomRight ),
+    stop: () => getDirectionMessage(Direction.Stop ),
 };
+
+function getDirectionMessage(payload: Direction) {
+    return getChannelMessageWithDelay(COMPONENT_KEY, { Direction: payload });
+}
 
 export default NeoPixelController;
