672ac156d048662310e5fa88d2a085e2e01bfc2a
ritesh singh <trex@riteshs-MBP.bbrouter>
2024-03-21 17:08:50 +0530

    added result process servo

--- src/modules/playground/components/simulated-hardwares/components/servo-motor/ServoMotor.tsx
+++ src/modules/playground/components/simulated-hardwares/components/servo-motor/ServoMotor.tsx
@@ -2,6 +2,7 @@ import {FC, useEffect, useState} from "react";
 import {Button} from "@/modules/common/components/button/Button";
 import {usePlayground} from "@/modules/playground/providers/playground.provider";
 import {setState} from "blockly/core/utils/aria";
+import {resetMessageQueue} from "@/utils/pg-comm-channel.util";
 
 interface ServoMotorProps {
     initialPosition: number;
@@ -19,24 +20,6 @@ export const ServoMotor: FC<ServoMotorProps> = ({initialPosition, destinationPos
 
     const registerComponent = usePlayground()
 
-    function processResult() {
-
-    }
-
-    function isOnDestination(){
-
-    }
-
-    function turn(direction:ServoDirections) {
-        switch (direction) {
-            case ServoDirections.RIGHT:
-                turnRight()
-                break;
-            case ServoDirections.LEFT:
-                turnLeft()
-                break;
-        }
-    }
 
     useEffect(() => {
         setAngle(initialPosition)
@@ -49,6 +32,19 @@ export const ServoMotor: FC<ServoMotorProps> = ({initialPosition, destinationPos
         })
     }, []);
 
+
+   //process motion
+    function turn(direction:ServoDirections) {
+        switch (direction) {
+            case ServoDirections.RIGHT:
+                turnRight()
+                break;
+            case ServoDirections.LEFT:
+                turnLeft()
+                break;
+        }
+    }
+
     function turnRight(){
         setAngle(angle => (angle + 45) % 360);
     }
@@ -56,6 +52,41 @@ export const ServoMotor: FC<ServoMotorProps> = ({initialPosition, destinationPos
     function turnLeft(){
         setAngle(angle => (angle - 45) % 360);
     }
+
+
+    //result Processing
+    function processResult() {
+        if (isOnDestination) {
+            processSuccess();
+        } else {
+            processFailure('not on destination!');
+        }
+    }
+
+    function processSuccess() {
+        resetMessageQueue()
+        displayMessage('Completed!');
+        resetComponent()
+    }
+
+    function processFailure(reason: string) {
+        resetMessageQueue();
+        displayMessage("something went wrong! " + reason);
+        resetComponent()
+    }
+
+    function displayMessage(message: String) {
+        alert(message);
+    }
+
+
+    const isOnDestination  = initialPosition === destinationPosition
+
+    function resetComponent(){
+        setAngle(0)
+    }
+
+
 
 
     return (
--- src/modules/playground/components/simulated-hardwares/impl/hwResultHandler.ts
+++ src/modules/playground/components/simulated-hardwares/impl/hwResultHandler.ts
@@ -18,3 +18,7 @@ export abstract class HwResultHandler{
 
 
 }
+
+const commonFun = {
+     resetDisplay:()=>{}
+}
