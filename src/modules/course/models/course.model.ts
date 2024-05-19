2665d988ed54071c977ef4a733d1b154e98570dc
mrmilind11 <mrmilind11@gmail.com>
2024-05-19 12:49:55 +0530

    Added category apis

--- src/modules/course/models/course.model.ts
+++ src/modules/course/models/course.model.ts
@@ -4,10 +4,12 @@ export interface LessonModel {
   title: string;
   description: string;
   duration: number;
+  priority?: number;
 }
 
 export interface LessonGroupModel {
   id: number;
   title: string;
   lessonList: LessonModel[];
+  priority?: number;
 }
