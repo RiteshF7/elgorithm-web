640929a2fdebca4b11976ad993723f9272e4f2d9
T- Rex <t-rex@T-s-iMac.bbrouter>
2024-05-25 16:09:22 +0530

    added db

--- src/database/fauna-db.ts
+++ src/database/fauna-db.ts
@@ -1,22 +1,42 @@
 const faunadb = require('faunadb');
-const faunaClient = new faunadb.Client({
-    secret: process.env.FAUNA_SECRET
-});
+const client = new faunadb.Client({ secret: 'fnAFiP6nvdAAQDozVr_enfZPqeMOHd9qQApGXLH9',domain: 'db.fauna.com',
+    scheme: 'https', });
 const q = faunadb.query;
 
 
-
 export const getSnippet = async () => {
-    const {data} = await faunaClient.query(q.Map(q.Paginate(q.Documents(q.Collection('snippets'))), q.Lambda('ref', q.Get(q.Var('ref')))));
-    // @ts-ignore
-    const snippets = data.map( (snippet) =>{
-        snippet.id = snippet.ref.id;
-        delete snippet.ref;
-        return snippet;
-    })
-    console.log(snippets,"working!")
+    // const {data} = await faunaClient.query(q.Map(q.Paginate(q.Documents(q.Collection('snippets'))), q.Lambda('ref', q.Get(q.Var('ref')))));
+    // // @ts-ignore
+    // const snippets = data.map((snippet) => {
+    //     snippet.id = snippet.ref.id;
+    //     delete snippet.ref;
+    //     return snippet;
+    // })
+    // console.log(snippets, "working!")
 };
 
+export const getSnippets = async () => {
+    const data = client.query(
+        q.ToDate('2024-10-10')
+    )
+    console.log(data,' ::data!')
+    // const { data } = await faunaClient.query(
+    //     q.Map(
+    //         q.Paginate(q.Documents(q.Collection('snippets'))),
+    //         q.Lambda('ref', q.Get(q.Var('ref')))
+    //     )
+    // );
+    // // @ts-ignore
+    //
+    // const snippets = data.map((snippet) => {
+    //     snippet.id = snippet.ref.id;
+    //     delete snippet.ref;
+    //     return snippet;
+    // });
+    //
+    // console.log(snippets)
+    // return snippets;
+};
 
 // export const getFaunaDb = async () => {
 //     const result = await faunaClient.query(q.Paginate(q.Match(q.Index('all_playgrounds'))));
