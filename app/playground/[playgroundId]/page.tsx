b0d80a7d52f7481c9dab0c9160665f7e036df4f7
ritesh <riteshf7@gmail.com>
2024-09-04 15:55:18 +0530

    playground logic working

--- app/playground/[playgroundId]/page.tsx
+++ app/playground/[playgroundId]/page.tsx
@@ -5,19 +5,17 @@ import {PlaygroundContainerContent} from "@/content/banner-main/playground-conta
 import {useParams} from "next/navigation";
 
 
-const PlayGroundPage: FC = () => {
+const PlayGroundLessonPage:FC<{ params: { playgroundId: string } }> = ({ params })=> {
 
-    const { id } = useParams<{ id?: string }>();
-    const idValue = parseInt(id ?? '0', 10) || 0;
 
-    console.log(id,"some val:::")
+    console.log(params,"some val:::")
     return (
         <main className={'flex flex-col overflow-y-auto max-w-desktop px-2 py-4 mx-auto gap-12'}>
-            <div className={'p-4'}>
-                <PlayGroundContainer state={PlaygroundContainerContent[idValue]} />
-            </div>
-        </main >
-    )
+        <div className={'p-4'}>
+        <PlayGroundContainer state={PlaygroundContainerContent[parseInt(params.playgroundId)]} />
+    </div>
+    </main >
+)
 }
 
-export default PlayGroundPage;
+export default PlayGroundLessonPage;
