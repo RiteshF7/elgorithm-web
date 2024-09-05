60bbc6eb78080dc5deb6da6acd8b917707485468
ritesh <riteshf7@gmail.com>
2024-09-05 10:19:57 +0530

    added

--- app/stages/page.tsx
+++ app/stages/page.tsx
@@ -20,10 +20,12 @@ const getCategoryList = async (): Promise<CategoryModel[]> => {
 const SelectStagePage = () => {
     const [categoryList, setCategoryList] = useState<CategoryModel[]>([]);
     const bannerContent = {
-        title: 'Stay motivated',
-        description: 'Form a real learning habit with fun content that’s always well-paced, game-like progress tracking, and friendly reminders',
+        title: 'Explore These Exciting Robotics Projects!',
+        titleHighlight:"Robotics",
+        description: '',
+        actionText : "",
+        actionUrl:"",
         imageUrl: '/home/stay-motivated.svg',
-        imageAlt: 'Stay motivated'
     }
     useEffect(() => {
         getCategoryList().then(r => setCategoryList(r)).catch(e => console.log(e));
@@ -32,19 +34,18 @@ const SelectStagePage = () => {
     return (
 
         <main className="flex flex-col overflow-y-auto max-w-desktop px-2 py-4 mx-auto gap-4">
-            <div className="ml-5 order-first bg-gray-200 border border-gray-300 shadow-md rounded-lg">
-                <ContentCard {...bannerContent} direction="rtl"/>
-            </div>
 
-                <h1 className="h1 ml-5">Hardware Projects</h1>
+            <BannerMain {...bannerContent} ></BannerMain>
 
+            <div className={'mb-5'}/>
+            <h3 className="h3 ml-5">Hardware Projects</h3>
 
 
             <ProjectPage projects={projectList}/>
-                <h1 className="h1 ml-5">Virtual Projects</h1>
-                <ProjectPage projects={projectList}/>
+            <h1 className="h1 ml-5">Virtual Projects</h1>
+            <ProjectPage projects={projectList}/>
         </main>
-);
+    );
 }
 
 export default SelectStagePage;
