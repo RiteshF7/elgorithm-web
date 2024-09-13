5dc20cdec86e8e2f31559f10598b4a9946d3daf0
ritesh <riteshf7@gmail.com>
2024-09-13 11:37:03 +0530

    changed landing page image

--- src/content/banner-main/banner-main.content.ts
+++ src/content/banner-main/banner-main.content.ts
@@ -6,5 +6,5 @@ export const BannerMainContent: BannerMainProps = {
     description: ' Whether you\'re a beginner or an experienced engineer, our platform offers a variety of robotics projects that teach you the essentials of coding, electronics, and mechanical design. Learn by doing, and build real-world robots that solve real-world problems.',
     actionText: 'Get started',
     actionUrl: '/stages',
-    imageUrl: '/home/landing_3.jpeg'
+    imageUrl: '/home/landing_3.png'
 }
